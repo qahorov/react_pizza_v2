23a7c4e0672aac70a95acf53ef23a95911eb2cd0
alex <test.test@mail.ru>
2022-10-08 18:34:24 +0300

    useState, onClick, деструктуризация

--- src/App.js
+++ src/App.js
@@ -10,17 +10,17 @@ import './scss/app.scss';
 function App() {
   return (
     
-    <div class="wrapper">
+    <div className="wrapper">
       <div className="block1"></div>
       <Header />
-      <div class="content">
-        <div class="container">
-          <div class="content__top">
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
             <Categories />
             <Sort />
           </div>
-          <h2 class="content__title">Все пиццы</h2>
-          <div class="content__items">
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
             <PizzaBlock title="Мексиканская" price={500} />
             <PizzaBlock title="Обичная" price={350} />
           </div>
